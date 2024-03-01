a1e5121ac34e75ccd56aab748975f6670e22e451
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2024-03-01 17:59:27 -0400

    Created Service API Pokemon.

--- App.tsx
+++ App.tsx
@@ -2,5 +2,5 @@ import { HomePage } from "./src/pages/home/homePage";
 import { WelcomePage } from "./src/pages/welcome/welcomePage";
 
 export default function App() {
-  return <WelcomePage />;
+  return <HomePage />;
 }
--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, SafeAreaView, View } from "react-native";
 import { Header } from "../../components/header/header";
 import { styles_global } from "../../styles/fonts";
+import api from "../../service/api";
 
+type PokemonType = {
+  type: string;
+};
+
+type Pokemon = {
+  name: string;
+  url: string;
+  id: number;
+  types: PokemonType[];
+};
+
+type Request = {
+  id: number;
+  types: PokemonType[];
+};
 export function HomePage() {
-  console.log(typeof styles_global); //
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    async function getAllPokemons() {
+      const response = await api.get("/pokemon");
+      const { results } = response.data;
+
+      const payloadPokemons = await Promise.all(
+        results.map(async (pokemon: Pokemon) => {
+          const { id, types } = await getMoreInfo(pokemon.url);
+
+          return {
+            name: pokemon.name,
+            id,
+            types,
+          };
+        })
+      );
+      setPokemons(payloadPokemons);
+      console.log(payloadPokemons);
+    }
+    getAllPokemons();
+  }, []);
+
+  async function getMoreInfo(url: string): Promise<Request> {
+    const response = await api.get(url);
+    const { id, types } = response.data;
+    return { id, types };
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Header />
@@ -13,6 +58,9 @@ export function HomePage() {
         <Text style={styles_global.description}>
           Search for Pokémon by name or using the National Pokédex number.
         </Text>
+        {pokemons.map((item) => (
+          <Text>{item.name}</Text>
+        ))}
       </View>
     </SafeAreaView>
   );
